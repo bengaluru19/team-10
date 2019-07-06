231f4b58e78a405dd1c7ca32326a3d56770db8aa
KS <kaushiksingh001@gmail.com>
2019-07-07 03:19:41 +0530

    Auto email added

---
+++ Auto-Email/autoemail-ty.py
@@ -0,0 +1,9 @@
+# import yagmail module.
+import yagmail
+# connect to smtp server.
+yag_smtp_connection = yagmail.SMTP( user="codeforgoodteam101@gmail.com", password="", host='smtp.gmail.com')# email subject
+subject = 'Hello from richard'
+# email content with attached file path.
+contents = ['/home/kaushiksingh/team-10/Auto-Email/ty.html']
+# send the email
+yag_smtp_connection.send('kaushiksingh001@gmail.com', subject, contents)
